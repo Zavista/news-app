21d45338763888be4da00f180e63c162218c051e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 22:41:30 -0500

    Update NewsBoard to fetch articles

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 const NewsBoard = () => {
+
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.API_KEY}`;
+        fetch(url).then(res => res.json()).then(data => setArticles(data.articles))
+    },[])
   return (
-    <div>NewsBoard</div>
+    <div>
+        <h2 className="text-center mt-3">Latest <span className="badge bg-danger">News</span></h2>
+    </div>
   )
 }
 
