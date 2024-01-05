3921a5300828c61b1c76f65cab082a79f3367b1a
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 23:00:50 -0500

    Add NewsItem

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,17 +1,26 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import NewsItem from "./NewsItem"
+
+
 
 const NewsBoard = () => {
-
     const [articles, setArticles] = useState([])
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.API_KEY}`;
-        fetch(url).then(res => res.json()).then(data => setArticles(data.articles))
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url).then(res => res.json()).then(data => {
+            console.log(data); 
+            setArticles(data.articles); 
+        })
     },[])
+    
   return (
     <div>
         <h2 className="text-center mt-3">Latest <span className="badge bg-danger">News</span></h2>
+        {articles.map((news, index)=>{
+            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}></NewsItem>
+        })}
     </div>
   )
 }
