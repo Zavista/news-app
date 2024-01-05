c33492b44388d57c48c159e5ca4912f580f1a3fe
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 23:47:57 -0500

    Add default image

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,7 +1,9 @@
+import image from '../assets/news.jpeg'
+
 const NewsItem = ({title, description, src, url}) => {
   return (
     <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3" style={{maxWidth: "345px"}}>
-      <img src={src} style={{height:'200px', objectFit:"cover"}} className="card-img-top" alt="..."/>
+      <img src={src?src:image} style={{height:'200px', objectFit:"cover"}} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{title.length > 75 ? `${title.slice(0, 75)}...` : title}</h5>
         <p className="card-text">{description?description.slice(0,90):"No curent information on the event."}</p>
