46657ac538d1b27b11ee8c06940bd0e17caf51b8
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 22:27:25 -0500

    Add NewsBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import NewsBoard from "./components/NewsBoard"
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <NewsBoard></NewsBoard>
     </div>
   )
 }
