b5aa422b5b2f619ab0f3f2022cf2ae213f392ce6
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 23:44:11 -0500

    add reload articles when category changes

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -4,16 +4,16 @@ import NewsItem from "./NewsItem"
 
 
 
-const NewsBoard = () => {
+const NewsBoard = ( {category} ) => {
     const [articles, setArticles] = useState([])
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(res => res.json()).then(data => {
             console.log(data); 
             setArticles(data.articles); 
         })
-    },[])
+    },[category])
     
   return (
     <div>
