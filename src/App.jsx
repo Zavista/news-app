ca3439d8a2a6a6d958aacd283bb2b9d245851701
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 23:32:01 -0500

    Create category State

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar"
 import NewsBoard from "./components/NewsBoard"
+import { useState } from "react";
 
 const App = () => {
+
+  const [category, setCategory] = useState("General");
+
   return (
     <div>
-      <Navbar></Navbar>
-      <NewsBoard></NewsBoard>
+      <Navbar setCategory={setCategory}></Navbar>
+      <NewsBoard category={category}></NewsBoard>
     </div>
   )
 }
