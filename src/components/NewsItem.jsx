14055b2f35af5b3ad80d6dc40c3bed11820bd971
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-04 23:01:04 -0500

    Update NewsItem to include props

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,11 +1,11 @@
-const NewsItem = () => {
+const NewsItem = ({title, description, src, url}) => {
   return (
-    <div className="card" style="width: 18rem;">
-      <img src="..." className="card-img-top" alt="..."/>
+    <div className="card" style={{maxWidth: "345px"}}>
+      <img src={src} className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={url} className="btn btn-primary">Read More</a>
       </div>
     </div>
   )
